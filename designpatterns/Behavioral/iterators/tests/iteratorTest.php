c6da000bf4c228db18652ef4da73feba0b5f0d87
jim-nnamdi <jimsamuel50@gmail.com>
2020-10-22 14:10:23 +0100

    Installed PHPUnit for testing && set up unit tests for iterator class

---
+++ designpatterns/Behavioral/iterators/tests/iteratorTest.php
@@ -0,0 +1,15 @@
+<?php 
+
+declare(strict_types = 1);
+
+namespace designpatterns\behavioral\iterators\tests;
+
+use designpatterns\behavioral\iterators\Book;
+use designpatterns\behavioral\iterators\BookList;
+use PHPUnit\Framework\TestCase;
+
+class IteratorTest extends TestCase{
+    public function can_iterate_over_book_list(){
+        
+    }
+}
