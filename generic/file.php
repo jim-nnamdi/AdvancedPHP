50249aa51cffcf7fbc91fefee715b45b22b1a685
jim-nnamdi <jimsamuel50@gmail.com>
2020-11-18 16:58:49 +0100

    updated the file.php file and added cookies

--- generic/file.php
+++ generic/file.php
@@ -26,16 +26,33 @@ while (!feof($a_file)) {
 
 # upload file . 
 
-$target_dir = "uploads";
+$target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["filetoupload"]["name"]);
-$save_image = 1;
+$save_upload = 1;
 $image_extension = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+
+# check if image . 
 if (isset($_POST["submit"])) {
   $check = getimagesize($target_file, $_FILES["filetoupload"]["tmp_name"]);
-  if ($check)
-    echo "file is an image" .  $check['mime'];
-  $save_image = 1;
+  if ($check) {
+    echo "the file is an image" . $check["mime"];
+    $save_upload = 1;
+  }
+  if (file_exists($target_file)) {
+    echo "file already exists";
+    $save_upload = 0;
+  }
+  if (filesize($target_file) > 100) {
+    echo "file is too large, please choose a lesser file";
+    $save_upload = 0;
+  }
+
+  # upload file . 
+  if (move_uploaded_file($_FILES["filetoupload"]["tmp_name"], $target_file)) {
+    echo "file has been uploaded successfully to " . $target_dir;
+    $save_upload = 1;
+  }
 } else {
   echo "file is not an image";
-  $save_image = 0;
+  $save_upload = 0;
 }
